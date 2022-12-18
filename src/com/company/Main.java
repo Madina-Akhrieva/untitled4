584bf79df3c3261b7045975f6a2a92ffe2bf5069
Madina Akhrieva <madina.akhrieva@innowise-group.com>
2022-12-18 18:34:58 +0300

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] array = new int[4];//{0,0,0,0}
+        int arrayd[] = new int[5];
+
+        int arrrrr[] = {5,6,5,9};
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        arrrrr[0] = i;
+        int b = scanner.nextInt();
+        arrrrr[1] = b;
+
+        //5 6 5 6 5
+        String s = scanner.nextLine();
+
+
+
+        int[] array3 = {5, 6, 9, 4};
+        int[] array6 = new int[]{4, 5, 6, 7};
+
+        boolean[] boolArr = {true, false, true};
+        String[] strArray= {"sd", "sd"};
+
+        Object[] objects = {true, 5};
+
+
+
+
+    }
+
+
+}
